2ac8d64628e8285cb919e188e7d381a6c336ea34
Tim Klaus <timklaus@Tims-MacBook-Pro.local>
2020-02-24 13:11:15 -0800

    write branching rules for returning results and write script to clear previous result upon form submission.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,30 @@
 $(document).ready(function() {
   $("form#survey").submit(function(){
-  console.log("HI")
-  event.preventDefault();
-  var jsScore = parseInt($("#js-independant").val())+ parseInt($("#js-organize").val());
-  var pythonScore = parseInt($("#python-elegant").val()) + parseInt($("#python-comm").val());
-  var rubyScore = parseInt($("#ruby-start").val()) + parseInt($("#ruby-magic").val());
-  
+    // This will hide previous results upon form submission
+    $("#RonJavascript").hide();
+    $("#TomPython").hide();
+    $("#BenRuby").hide();
 
-  console.log("Your current Javacscript Score is:  " + jsScore);
-  console.log("Your current Python Score is:  " + pythonScore);
-  console.log("Your current Ruby score is:  " + rubyScore);
-  
+    // This will gather scores for each variable from user input
+    var jsScore = parseInt($("#js-independant").val())+ parseInt($("#js-organize").val());
+    var pythonScore = parseInt($("#python-elegant").val()) + parseInt($("#python-comm").val());
+    var rubyScore = parseInt($("#ruby-start").val()) + parseInt($("#ruby-magic").val());
+    
+    //This will check that scores for each variable are being tallied successfully.
+    console.log("Your current Javacscript Score is:  " + jsScore);
+    console.log("Your current Python Score is:  " + pythonScore);
+    console.log("Your current Ruby score is:  " + rubyScore);
+    
+// debugger
+    //This branching will determine results based on scores
+    if (jsScore >= pythonScore && jsScore>= rubyScore) {
+      $("#RonJavascript").show();
+    } else if (pythonScore > jsScore && pythonScore > rubyScore) {
+      $("#TomPython").show();
+    } else if (rubyScore > jsScore && rubyScore > pythonScore) {
+      $("#BenRuby").show();
+    }
+    event.preventDefault();
     
   });
 });
