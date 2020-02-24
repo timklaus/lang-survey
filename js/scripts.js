25c8ed965a3a0d83c13ee1807eeb3b5f47c308d5
Tim Klaus <timklaus@Tims-MacBook-Pro.local>
2020-02-24 12:47:46 -0800

    debug form submission error

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 $(document).ready(function() {
   $("form#survey").submit(function(){
-debugger
-    var jsScore = parseInt($("#js-independant").val())+ parseInt($("#js-organize").val());
-    var pythonScore = parseInt($("#python-elegant").val()) + parseInt($("#python-comm").val());
-    var rubyScore = parseInt($("#ruby-start").val()) + parseInt($("#ruby-magic").val());
+  console.log("HI")
+  event.preventDefault();
+  var jsScore = parseInt($("#js-independant").val())+ parseInt($("#js-organize").val());
+  var pythonScore = parseInt($("#python-elegant").val()) + parseInt($("#python-comm").val());
+  var rubyScore = parseInt($("#ruby-start").val()) + parseInt($("#ruby-magic").val());
   
 
-    console.log("Your current Javacscript Score is:  " + jsScore);
-    console.log("Your current Python Score is:  " + pythonScore);
-    console.log("Your current Ruby score is:  " + rubyScore);
+  console.log("Your current Javacscript Score is:  " + jsScore);
+  console.log("Your current Python Score is:  " + pythonScore);
+  console.log("Your current Ruby score is:  " + rubyScore);
   
     
-    event.preventDefault();
   });
 });
